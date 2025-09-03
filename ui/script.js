26a8e686156c7d8e31b85f92e0c3d27f099e07ca
benal01 <bja4245@rit.edu>
2025-09-02 23:02:44 -0400

    web text based app setup and basic styling for testing

--- ui/script.js
+++ ui/script.js
@@ -1,6 +1,39 @@
-// Replace 'localhost:8080' with your server's host and port if different
 let socket = new WebSocket('ws://localhost:8080/user');
+let messages = []; // {message = type}
+const textArea = document.getElementById('textArea');
 
+//message, system
+output = function(message, type) {
+    messages[messages.length] = [message, type];
+    textArea.innerHTML += '<div class="output '+type+'">' + message + ' <span class="timestamp">' + new Date().toLocaleTimeString() + '</span><br> </div>';
+}
+
+
+
+output("One", "message");
+output("Two", "message");
+output("Three", "message");
+output("Four", "message");
+
+
+output("System message", "system");
+
+
+document.getElementById('loginBtn').onclick = function() {
+    const username = document.getElementById('username').value;
+    socket.send(username);
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('chat').style.display = 'block';
+    
+}
+
+document.getElementById('sendBtn').onclick = function() {
+    const message = document.getElementById('messageInput').value;
+    console.log('Sending to server:', message);
+    socket.send(message);
+}
+
+// Websocket connection
 socket.onopen = function(event) {
     console.log('WebSocket connection opened');
 };
@@ -16,17 +49,3 @@ socket.onclose = function(event) {
 socket.onerror = function(error) {
     console.error('WebSocket error:', error);
 };
-
-document.getElementById('loginBtn').onclick = function() {
-    const username = document.getElementById('username').value;
-    socket.send(username);
-    document.getElementById('login').style.display = 'none';
-    document.getElementById('chat').style.display = 'block';
-    
-}
-
-document.getElementById('sendBtn').onclick = function() {
-    const message = document.getElementById('messageInput').value;
-    console.log('Sending to server:', message);
-    socket.send(message);
-}
